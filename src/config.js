3ceda1a752da3261019878aee93b9e3f8cd3ca2c
aitor c. urrutia aranburu <aitorcurrutia@gmail.com>
2025-11-03 11:02:37 +0100

    content: edukia eguneratzen du

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "txarli.dev";
+export const SITE_DESCRIPTION = "Kaixo! Aitor naiz 🙋‍♂️";
